c4e231eb09f7ec496e885250c6018045862bea3c
Sorikairo <sorikairox@gmail.com>
2022-10-21 21:06:00 +0900

    style: deno lint

--- deps.ts
+++ deps.ts
@@ -2,4 +2,4 @@ export { rmdir } from 'https://deno.land/std/node/fs/promises.ts';
 export { existsSync } from 'https://deno.land/std/fs/mod.ts';
 export { Logger } from 'https://deno.land/x/danet/src/logger.ts';
 export { toPathString } from 'https://deno.land/std/fs/_util.ts';
-export { Command } from "https://deno.land/x/cliffy@v0.25.2/command/mod.ts";
+export { Command } from 'https://deno.land/x/cliffy@v0.25.2/command/mod.ts';
