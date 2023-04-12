a062da2c65a4a757204017a337a170c9a1d8a14f
Sorikairo <sorikairox@gmail.com>
2023-04-12 21:53:04 +0900

    fix: mkdir bundle folder before bundling

--- bundle.ts
+++ bundle.ts
@@ -1,4 +1,5 @@
 export const bundleProject = async (options: null, name: string) => {
+    await Deno.mkdir('./bundle');
     const p = Deno.run({
         cmd: ['deno', 'bundle', 'run.ts', `./bundle/${name}`]
     })
