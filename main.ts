dfcd622d5e5bd46662b5f392803fac65cb5415c2
Snider <snider@lt.hn>
2023-04-12 14:50:03 +0100

    Adds help command to show help doc for sub commands `danet help new`

--- main.ts
+++ main.ts
@@ -1,13 +1,14 @@
-import { Command } from './deps.ts';
+import { Command, HelpCommand } from './deps.ts';
 import { generateProject } from './generate.ts';
 import { runProject, runProjectWithWatch } from "./run.ts";
 import { bundleProject } from "./bundle.ts";
 
-const program	 = new Command().name('danet')
+const program = new Command().name('danet')
 			.description('Danet CLI Interface')
 			.meta("deno", Deno.version.deno)
 			.meta("v8", Deno.version.v8)
-			.meta("typescript", Deno.version.typescript);
+			.meta("typescript", Deno.version.typescript)
+			.command("help", new HelpCommand().global());
 
 program.command('new').arguments('<name:string>')
 	.option('--postgres', 'Setup project with postgres code')
