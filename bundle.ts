3d9a3198199236a9c6ba721cfbb31641e331ec5a
Marco Antônio <ma.souza.junior@gmail.com>
2024-04-29 15:07:26 -0300

    fix: check if bundlePath exists before removing it

--- bundle.ts
+++ bundle.ts
@@ -1,8 +1,24 @@
-export const bundleProject = async (options: { entrypoint: string }, name: string) => {
-    await Deno.remove("./bundle", { recursive: true });
-    await Deno.mkdir('./bundle');
-    const p = Deno.run({
-        cmd: ['deno', 'bundle', options.entrypoint, `./bundle/${name}`]
-    })
-    await p.status();
+const bundlePath = './bundle';
+
+function bundleExists() {
+	try {
+		Deno.statSync(bundlePath);
+		return true;
+	} catch (_e) {
+		return false;
+	}
 }
+
+export const bundleProject = async (
+	options: { entrypoint: string },
+	name: string,
+) => {
+	if (bundleExists()) {
+		await Deno.remove(bundlePath, { recursive: true });
+	}
+	await Deno.mkdir(bundlePath);
+	const p = Deno.run({
+		cmd: ['deno', 'bundle', options.entrypoint, `${bundlePath}/${name}`],
+	});
+	await p.status();
+};
