d341f4920f098d7d42ba8bc4ca5afc05eabe8c8a
Marco Antônio <ma.souza.junior@gmail.com>
2024-05-02 09:47:33 -0300

    enh: make bundle path configurable

--- bundle.ts
+++ bundle.ts
@@ -1,19 +1,17 @@
 
-const bundlePath = './bundle';
-
-const bundleFolderExists = (folder: string) => 
+const bundleFolderExists = (folder: string) =>
 	Deno.stat(folder)
 		.then(() => true)
 		.catch(() => false);
 
 export const bundleProject = async (
-	options: { entrypoint: string },
+	options: { entrypoint: string, bundlePath = './bundle' },
 	name: string,
 ) => {
 	if (await bundleExists(bundlePath)) {
 		await Deno.remove(bundlePath, { recursive: true });
 	}
-	
+
 	await Deno.mkdir(bundlePath);
 	const p = Deno.run({
 		cmd: ['deno', 'bundle', options.entrypoint, `${bundlePath}/${name}`],
