7c16dad5d71ff174aed722396ffa076956711cc0
Marco Antônio <ma.souza.junior@gmail.com>
2024-05-02 09:42:40 -0300

    enh: use promise

--- bundle.ts
+++ bundle.ts
@@ -1,21 +1,19 @@
+
 const bundlePath = './bundle';
 
-function bundleExists() {
-	try {
-		Deno.statSync(bundlePath);
-		return true;
-	} catch (_e) {
-		return false;
-	}
-}
+const bundleFolderExists = (folder: string) => 
+	Deno.stat(folder)
+		.then(() => true)
+		.catch(() => false);
 
 export const bundleProject = async (
 	options: { entrypoint: string },
 	name: string,
 ) => {
-	if (bundleExists()) {
+	if (await bundleExists(bundlePath)) {
 		await Deno.remove(bundlePath, { recursive: true });
 	}
+	
 	await Deno.mkdir(bundlePath);
 	const p = Deno.run({
 		cmd: ['deno', 'bundle', options.entrypoint, `${bundlePath}/${name}`],
