9e78effab9148374e6ff61c12ef0a98fbf0c335f
Thomas Cruveilher <38007824+Sorikairox@users.noreply.github.com>
2024-10-01 15:18:37 +0000

    fix: stdout running app to console

--- run.ts
+++ run.ts
@@ -1,13 +1,15 @@
 export const runProjectWithWatch: () => void = async () => {
     const command = new Deno.Command("deno", {
-        args: ['run', '--watch', '--allow-net', '--allow-read',  '--unstable',  '--allow-env', 'run.ts']
+        args: ['run', '--watch', '--allow-net', '--allow-read',  '--unstable',  '--allow-env', 'run.ts'],
+        stdout: "inherit"
     });
     await command.output();
 }
 
 export const runProject: () => void = async () => {
     const command = new Deno.Command("deno", {
-        args: ['run', '--allow-net', '--allow-read',  '--unstable',  '--allow-env', 'run.ts']
+        args: ['run', '--allow-net', '--allow-read',  '--unstable',  '--allow-env', 'run.ts'],
+        stdout: "inherit"
     });
     await command.output();
 }
