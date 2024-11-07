2601d8e14f798e3b4d04f44c3a3ed7ab4dc2da31
Sorikairo <sorikairox@gmail.com>
2024-11-07 20:21:29 +0900

    chore: version for esbuild dependencies

--- bundle.ts
+++ bundle.ts
@@ -1,5 +1,5 @@
-import * as esbuild from "npm:esbuild";
-import { denoPlugins } from "jsr:@luca/esbuild-deno-loader";
+import * as esbuild from "npm:esbuild@0.20.2";
+import { denoPlugins } from "jsr:@luca/esbuild-deno-loader@0.11.0";
 
 
 
